aa88632915bb9a9f63c9dd2f2d2d83b275e15986
Saif <saif.mukadam@ril.com>
2024-05-15 16:35:26 +0530

    bh

--- Ai Analyzer/HIstoryViewController.swift
+++ Ai Analyzer/HIstoryViewController.swift
@@ -83,7 +83,7 @@ extension UITableView {
     func setEmptyMessage(_ message: String) {
         let messageLabel = UILabel(frame: CGRect(x: 0, y: 0, width: self.bounds.size.width, height: self.bounds.size.height))
         messageLabel.text = message
-        messageLabel.textColor = .black
+        messageLabel.textColor = .gray
         messageLabel.numberOfLines = 0
         messageLabel.textAlignment = .center
         messageLabel.font = UIFont(name: "TrebuchetMS", size: 15)
